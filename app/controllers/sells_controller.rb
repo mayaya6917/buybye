54a3724c5f728714ad6d5c5870537954c9b44a13
mayaya6917 <maya.k03@gmail.com>
2024-01-21 18:33:37 +0900

    出品データ投稿機能

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -3,4 +3,30 @@ class SellsController < ApplicationController
     @sells = Sell.all
   end
 
+  def new
+    @sell = Sell.new
+  end
+
+  def create
+    if current_user
+      @sell = current_user.sells.new(sell_params)
+  
+      if @sell.save
+        redirect_to "/"
+      else
+        puts "Failed to save:"
+        puts @sell.errors.full_messages
+        render :new, status: :unprocessable_entity
+      end
+    else
+      flash[:alert] = "ログインしてください"
+      redirect_to user_session_path
+    end
+  end
+
+  private
+  def sell_params
+    params.require(:sell).permit(:item, :memo, :status_id, :price, :wish, :limit, :image).merge(user_id: current_user.id)
+  end
+
 end
