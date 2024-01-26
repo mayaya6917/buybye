3b0aec6476efde62fbda28ef1515b5f417c70842
mayaya6917 <maya.k03@gmail.com>
2024-01-26 16:54:48 +0900

    削除、詳細、編集機能

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -1,32 +1,65 @@
 class SellsController < ApplicationController
+  before_action :set_sell, only: [:edit, :update, :destroy]
+
   def index
-    @sells = Sell.all
+    @sells = current_user.sells
   end
 
   def new
-    @sell = Sell.new
+    @sell = current_user.sells.new
   end
 
   def create
-    if current_user
-      @sell = current_user.sells.new(sell_params)
-  
-      if @sell.save
-        redirect_to "/"
-      else
-        puts "Failed to save:"
-        puts @sell.errors.full_messages
-        render :new, status: :unprocessable_entity
-      end
+    @sell = current_user.sells.new(sell_params)
+
+    if @sell.save
+      redirect_to "/"
     else
-      flash[:alert] = "ログインしてください"
-      redirect_to user_session_path
+      puts "Failed to save:"
+      puts @sell.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @sell
+      @sell.destroy
+      redirect_to "/"
+    else
+      flash[:alert] = "対象の投稿が見つかりませんでした"
+      redirect_to sells_path
+    end
+  end
+
+  def show
+    @sell = current_user.sells.find_by(id: params[:id])
+  end
+
+  def edit
+    @sell = current_user.sells.find_by(id: params[:id])
+  end
+
+  def update
+    if @sell.update(sell_params)
+      redirect_to root_path
+    else
+      flash[:alert] = "更新に失敗しました"
+      render :edit
     end
   end
 
   private
+
   def sell_params
-    params.require(:sell).permit(:item, :memo, :status_id, :price, :wish, :limit, :image).merge(user_id: current_user.id)
+    params.require(:sell).permit(:item, :memo, :status_id, :price, :wish, :limit, :image)
+  end
+
+  def set_sell
+    @sell = current_user.sells.find_by(id: params[:id])
+    unless @sell
+      flash[:alert] = "対象の投稿が見つかりませんでした"
+      redirect_to sells_path
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'sells/index'
   devise_for :users
   root to: "sells#index"
-  resources :sells, only: [:index, :new, :create]
+  resources :sells
 end
